8a242b55d8ee130c92c1766be05d6c3deddbd683
Abhishek Dere <abhishekdere3004@gmail.com>
2023-11-28 10:49:03 +0530

    working done on registration page

--- Component/registrationpage/index.jsx
+++ Component/registrationpage/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
-export default function () {
+import styles from './styles.module.css'
+export default function Registrationpage() {
   return (
-    <div></div>
+    <div className={styles.container}>
+      <h1 className={styles.title}> Registration page</h1>
+      <div className={styles.inputs}>
+        <input className={styles.username} type='text' placeholder='USERNAME' />
+        <input className={styles.email} type='email' placeholder='EMAIL' />
+        <input className={styles.password} type='password' placeholder='PASSWORD' />
+        <input className={styles.password} type='password' placeholder='CONFIM PASSWORD' />
+      </div>
+      <button className={styles.button}><h2>LOGIN</h2></button>
+      <p className={styles.createAccount}>Already Have An Account ?</p>
+    </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Loginpage from '../../Component/loginpage'
+import Registrationpage from '../../Component/registrationpage'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     <Loginpage/>
+     <Registrationpage/>
     </main>
   )
 }
